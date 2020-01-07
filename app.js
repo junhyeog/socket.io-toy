7d8d925c23557ca8e269db0304b9d34455308acf
unknown <junhyeog@korea.ac.kr>
2020-01-07 17:43:46 +0900

    server set up

---
+++ app.js
@@ -0,0 +1,42 @@
+const backPort = 4000;
+
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// client가 socket.io server(현 서버의 'io')에 접속하면 connection event 발생
+// connection event가 발생했을 때의 event handler - callback의 인자의 sockket이 들어온다.
+// *socket (object): 각 clinet와의 interacting을 위한 객체
+// *io (object) : 연결된 모든 client와의 interacting을 위한 객체
+
+var numUsers = 0; // 전체 사람 수
+
+io.on('connection', (socket) => {
+
+  // on (method): ('event_name', event handler)
+
+  // client login query
+  socket.on('login', (data) => { // data={name, _id, }
+    console.log(`logged in client:${data.name} (${data._id})`);
+    //socket 객체에 client의 정보 저장
+    socket.name = data.name;
+    socket._id = data._id;
+    // 자신을 제외한 모든 client에게 login했다고 알림
+    socket.broadcast.emit('login', { name: soket.name, _id: socket._id });
+  });
+
+  // client로부터 메시지를 송신받음
+  socket.on('chat', (data) => {
+    console.log(`From ${socket.name}: ${data.msg}`);
+    // 자신을 포함한 모든 client에게 메시지 전송
+    io.emit('chat', { name: socket.name, _id: socket._id, msg: data.msg });
+  });
+});
+
+server.listen(backPort, () => {
+  console.log(`socket.io server listening on port ${backPort}`);
+});
